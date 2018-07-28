034d611fb1b4faaf0b6e0bd999244c8e026b51d9
eslamsherif <eng.islam.sherif@gmail.com>
2018-07-28 22:40:52 +0200

    Implement Prediction Step

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -13,7 +13,7 @@ using std::vector;
 /* Private Debug Functions */
 #ifdef DEBUG_GENERAL
 
-static void PrintMatrix(string str, MatrixXd prnt)
+static void PrintMatrix(string str, const MatrixXd prnt)
 {
     /* IOFormat inspired from https://eigen.tuxfamily.org/dox/structEigen_1_1IOFormat.html */
     const Eigen::IOFormat fmt(4, 0, ",\t", "", "\n\t[", "]");
@@ -22,7 +22,7 @@ static void PrintMatrix(string str, MatrixXd prnt)
     cout << endl;
 }
 
-static void PrintVector(string str, VectorXd prnt)
+static void PrintVector(string str, const VectorXd prnt)
 {
     /* IOFormat inspired from https://eigen.tuxfamily.org/dox/structEigen_1_1IOFormat.html */
     const Eigen::IOFormat fmt(4, 0, ",\t", "", "\n\t[", "]");
@@ -49,6 +49,101 @@ static void calculateMeasCovarMatrix(MatrixXd & __R_Lidar, MatrixXd & __R_Radar)
     __R_Radar(0U, 0U) = ( RADAR_STD_RHO * RADAR_STD_RHO );
     __R_Radar(1U, 1U) = ( RADAR_STD_PHI * RADAR_STD_PHI );
     __R_Radar(2U, 2U) = ( RADAR_STD_RHODOT * RADAR_STD_RHODOT );
+}
+
+static double correctAnglePhi(double Phi)
+{
+    /* TODO: Evaluate difference between lecture and my implementation */
+    while (Phi >  M_PI) Phi -= 2.0 * M_PI;
+    while (Phi < -M_PI) Phi += 2.0 * M_PI;
+
+    return Phi;
+    // while ( Phi > M_PI || Phi < -M_PI )
+    // {
+        // if ( Phi > M_PI )
+        // {
+            // Phi -= M_PI;
+        // }
+        // else
+        // {
+            // Phi += M_PI;
+        // }
+    // }
+
+    // return Phi;
+}
+
+static void generate_Sigma_Points(const VectorXd & __x, const MatrixXd & __P, MatrixXd & __out)
+{
+    VectorXd x_aug = VectorXd(AUG_DCNT);
+    MatrixXd P_aug = MatrixXd(AUG_DCNT, AUG_DCNT);
+
+    unsigned char temp = (STATE_DCNT + 1U);
+
+    x_aug.head(STATE_DCNT) = __x; /* Original State         */
+    x_aug(STATE_DCNT)      = 0U;  /* Acceleration Noise     */
+    x_aug(temp)            = 0U;  /* Yaw acceleration Noise */
+
+    P_aug.fill(0.0);
+    P_aug.topLeftCorner(STATE_DCNT,STATE_DCNT) = __P;
+    P_aug(STATE_DCNT,STATE_DCNT)               = PRCSS_LONG_ACC * PRCSS_LONG_ACC;
+    P_aug(temp, temp)                          = PRCSS_YAW_ACC * PRCSS_YAW_ACC;
+
+    MatrixXd L = P_aug.llt().matrixL();
+    double factor = sqrt(LAMDA + AUG_DCNT);
+
+    __out.col(0)  = x_aug;
+    for (unsigned char i = 0; i< AUG_DCNT; i++)
+    {
+        __out.col(i + 1U)            = x_aug + (factor * L.col(i));
+        __out.col(i + 1U + AUG_DCNT) = x_aug - (factor * L.col(i));
+    }
+}
+
+static void predict_Sigma_Points(const MatrixXd & __SigPts, const double __dt, MatrixXd & __out)
+{
+    double half_dt_sq = 0.5 * __dt * __dt;
+
+    for(unsigned char i = 0U; i < SIG_PTS_CNT; i++)
+    {
+        /* double Px       = __SigPts(0,i); */
+        /* double Py       = __SigPts(1,i); */
+        double V        = __SigPts(2,i);
+        double Psi      = __SigPts(3,i);
+        double Psidot   = __SigPts(4,i);
+        double Nua      = __SigPts(5,i);
+        double NuPsidd  = __SigPts(6,i);
+
+        VectorXd ProcessUpdate(STATE_DCNT);
+        VectorXd NoiseUpdate(STATE_DCNT);
+
+        /* avoid division by zero */
+        if(EPSI > fabs(Psidot)) /* Psi dot is zero */
+        {
+            ProcessUpdate(0) = V * cos(Psi) * __dt;
+            ProcessUpdate(1) = V * sin(Psi) * __dt;
+        }
+        else /* Psi dot is not zero */
+        {
+            double TurnRateFactor = V / Psidot;
+            double temp = Psi + (Psidot * __dt);
+
+            ProcessUpdate(0) = TurnRateFactor * ( (sin( temp )) - (sin(Psi)) );
+            ProcessUpdate(1) = TurnRateFactor * ( (cos(Psi)) - (cos( temp )) );
+        }
+
+        ProcessUpdate(2) = 0U;
+        ProcessUpdate(3) = Psidot * __dt;
+        ProcessUpdate(4) = 0U;
+
+        NoiseUpdate(0) = half_dt_sq * cos(Psi) * Nua;
+        NoiseUpdate(1) = half_dt_sq * sin(Psi) * Nua;
+        NoiseUpdate(2) = __dt       * Nua;
+        NoiseUpdate(3) = half_dt_sq * NuPsidd;
+        NoiseUpdate(4) = __dt       * NuPsidd;
+
+        __out.col(i) = __SigPts.col(i).head(STATE_DCNT) + ProcessUpdate + NoiseUpdate;
+    }
 }
 
 /* Public Functions */
@@ -160,7 +255,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package)
         Prediction(dt);
 
         #ifdef DEBUG_PREDICTION
-        cout << "------------------------------DTP0002------------------------------" << endl;
+        cout << "------------------------------DTP1001------------------------------" << endl;
         cout << "delta time = " << dt << endl;
         #endif
 
@@ -171,11 +266,13 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package)
         if(meas_package.sensor_type_ == MeasurementPackage::LASER)
         {
             #ifdef ENABLE_LASER
+            UpdateLidar(meas_package);
             #endif
         }
         else if(meas_package.sensor_type_ == MeasurementPackage::RADAR)
         {
             #ifdef ENABLE_RADAR
+            UpdateRadar(meas_package);
             #endif
         }
     }
@@ -188,12 +285,56 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package)
  */
 void UKF::Prediction(double delta_t)
 {
-    /**
-    TODO:
+    /*****************************************************************************
+     *  Generate Sigma Points
+     ****************************************************************************/
 
-    Complete this function! Estimate the object's location. Modify the state
-    vector, x_. Predict sigma points, the state, and the state covariance matrix.
-    */
+    MatrixXd SigPts = MatrixXd(AUG_DCNT, SIG_PTS_CNT);
+    SigPts.fill(0.0);
+
+    generate_Sigma_Points(x_, P_, SigPts);
+
+    #ifdef DEBUG_PREDICTION
+    cout << "------------------------------DTP2001------------------------------" << endl;
+    PrintMatrix("Sigma Points: ", SigPts);
+    #endif
+
+    /*****************************************************************************
+     *  Predict Sigma Points
+     ****************************************************************************/
+
+    predict_Sigma_Points(SigPts, delta_t, Xsig_pred_);
+
+    #ifdef DEBUG_PREDICTION
+    cout << "------------------------------DTP2002------------------------------" << endl;
+    PrintMatrix("Predicted Sigma Points: ", Xsig_pred_);
+    #endif
+
+    /*****************************************************************************
+     *  Predict Mean and Covariance
+     ****************************************************************************/
+
+    x_.fill(0.0);
+    P_.fill(0.0);
+
+    for(unsigned int i = 0; i < SIG_PTS_CNT; i++)
+    {
+        x_ = x_ + ( weights_(i) * Xsig_pred_.col(i) );
+    }
+
+    for(unsigned int i = 0; i < SIG_PTS_CNT; i++)
+    {
+        VectorXd temp = Xsig_pred_.col(i) - x_;
+        temp(3) = correctAnglePhi( temp(3) );
+
+        P_ = P_ + ( weights_(i) * temp * temp.transpose() );
+    }
+
+    #ifdef DEBUG_PREDICTION
+    cout << "------------------------------DTP2003------------------------------" << endl;
+    PrintVector("Predicted State: ", x_);
+    PrintMatrix("Predicted Covariance Matrix: ", P_);
+    #endif
 }
 
 /**
