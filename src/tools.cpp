d94085edc6ab56395eef73e58aaed8860b70c299
eslamsherif <eng.islam.sherif@gmail.com>
2018-07-28 15:37:16 +0200

    Update RMSE Calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,27 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+   VectorXd rmse(4);
+   rmse << 0,0,0,0;
+
+   /* check the validity of the inputs, Sanity Checks */
+   if ( ( estimations.size() == 0 ) || ( estimations.size() != ground_truth.size() ) )
+   {
+      return rmse;
+   }
+
+   /* Accumulate squared residuals */
+   for(unsigned int idx=0; idx < estimations.size(); ++idx){
+      VectorXd temp = estimations[idx] - ground_truth[idx];
+      temp = temp.array() * temp.array();
+      rmse += temp;
+   }
+
+   /* Calculate the mean */
+   rmse /= estimations.size();
+
+   /* Calculate the squared root */
+   rmse = rmse.array().sqrt();
+
+   return rmse;
 }
